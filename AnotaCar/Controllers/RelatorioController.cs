66ff9038f43ee13dbfd6a1ef4c55efbc5860cee6
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2021-05-20 22:43:09 -0300

    Iniciando modelagem do dashboard.

--- AnotaCar/Controllers/RelatorioController.cs
+++ AnotaCar/Controllers/RelatorioController.cs
@@ -1,15 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using AnotaCar.Data;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace AnotaCar.Controllers
 {
     public class RelatorioController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public RelatorioController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var labels = new List<string>() { "January", "february", "March", "April", "May", "June", "July" };
+
+            ViewBag.meses = labels;
+            ViewBag.TotalKM = "12";
             return View();
         }
     }
